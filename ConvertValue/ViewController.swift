240e20fe9f2e417c1a9e585b4ea3476230593f64
Charles Findlay <charlesfindlay2000-github@yahoo.com>
2015-09-21 14:27:42 -0400

    Hardcoded answer to show on button press.

--- ConvertValue/ViewController.swift
+++ ConvertValue/ViewController.swift
@@ -9,6 +9,9 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    
+    @IBOutlet weak var showAnswerHere: UILabel!
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -20,6 +23,12 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    
+    @IBAction func pressForAnswer(sender: AnyObject) {
+        let answer = "10 inches is 254 centimeters"
+        showAnswerHere.text = answer
+    }
+    
 
 }
 
